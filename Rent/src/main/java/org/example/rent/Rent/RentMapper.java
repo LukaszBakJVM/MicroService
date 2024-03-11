3663c1ddc91e214a6bfae0070929c461f19b2373
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-03-11 22:47:36 +0100

    Find rented cars

--- Rent/src/main/java/org/example/rent/Rent/RentMapper.java
+++ Rent/src/main/java/org/example/rent/Rent/RentMapper.java
@@ -2,31 +2,21 @@ package org.example.rent.Rent;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class RentMapper {
-    Rent dtoToEntity(RentDto dto){
+    Rent dtoToEntity(RentDto dto) {
         Rent rent = new Rent();
         rent.setCarId(dto.carId());
         rent.setClientId(dto.clientId());
         rent.setAmount(dto.price());
         return rent;
     }
-    RentDto entityToDto(Rent rent){
-        return new RentDto(rent.getCarId(), rent.getClientId(),rent.getAmount());
-    }
-    FindAllCars allCars(Rent rent){
-        List<Long>allCars = new ArrayList<>();
-        allCars.add(rent.getCarId());
 
-
-
-        return new FindAllCars(rent.getClientId(),allCars );
+    RentDto entityToDto(Rent rent) {
+        return new RentDto(rent.getCarId(), rent.getClientId(), rent.getAmount());
     }
 
 
-
-    }
+}
 
