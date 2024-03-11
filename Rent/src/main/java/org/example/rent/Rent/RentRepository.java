15fd4d785989fca94d48fcd67af72e04a6fcabad
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-03-11 21:33:13 +0100

    Find rented cars

--- Rent/src/main/java/org/example/rent/Rent/RentRepository.java
+++ Rent/src/main/java/org/example/rent/Rent/RentRepository.java
@@ -2,5 +2,9 @@ package org.example.rent.Rent;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RentRepository extends JpaRepository<Rent,Long> {
+    List<Rent>findAllByCarId(long id);
+    List<Rent>findAllByClientId(long id);
 }
